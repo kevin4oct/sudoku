30be91e5e34d6b19f56c4f9954e0b1c18dc65ff8
kevin <kevin4oct@163.com>
2018-12-15 17:38:52 +0800

    完善棋盘代码

--- app/src/main/java/com/geren/sudoku/main/view/MainActivity.java
+++ app/src/main/java/com/geren/sudoku/main/view/MainActivity.java
@@ -1,18 +1,32 @@
 package com.geren.sudoku.main.view;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.geren.sudoku.R;
 import com.geren.sudoku.bean.SudokuBean;
 import com.geren.sudoku.main.presenter.MainPresenter;
+import com.geren.sudoku.view.ChessBoard;
 
-public class MainActivity extends AppCompatActivity implements IMainView {
+public class MainActivity extends AppCompatActivity implements IMainView, Handler.Callback, View.OnClickListener {
 
+    public static final int TIME_START = 0x001;
+    public static final int TIME_GOON = 0x002;
+    public static final int TIME_STOP = 0x003;
     public static final String TAG = "我是MainActivity";
+    Handler mHandler = new Handler(this);
 
     private MainPresenter mPresenter;
+    private Button timeBtn;
+
+    private int time;
+    private ChessBoard chessBoard;
+    private Button returnBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +43,52 @@ public class MainActivity extends AppCompatActivity implements IMainView {
     }
 
     private void initView() {
-
+        returnBtn = (Button) findViewById(R.id.btn_return);
+        timeBtn = (Button) findViewById(R.id.btn_time);
+        chessBoard = (ChessBoard) findViewById(R.id.chessboard);
     }
 
     private void initListener() {
-
+        returnBtn.setOnClickListener(this);
     }
 
     private void initData() {
         mPresenter = new MainPresenter(this);
         mPresenter.initData();
+        mHandler.sendEmptyMessage(TIME_START);
     }
 
     @Override
     public void setData(SudokuBean[][] data) {
         Log.e(TAG, "data>>>" + data);
+        chessBoard.setData(data);
+    }
+
+    @Override
+    public boolean handleMessage(Message msg) {
+        switch (msg.what) {
+            case TIME_START:
+                time += 2;
+                timeBtn.setText(time + "(s)");
+                mHandler.sendEmptyMessageDelayed(TIME_START, 2000);
+                break;
+            case TIME_GOON:
+
+                break;
+            case TIME_STOP:
+
+                break;
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_return:
+                this.finish();
+                break;
+        }
     }
 }
--- app/src/main/java/com/geren/sudoku/view/ChessBoard.java
+++ app/src/main/java/com/geren/sudoku/view/ChessBoard.java
@@ -2,33 +2,133 @@ package com.geren.sudoku.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.geren.sudoku.bean.SudokuBean;
+
+import static android.graphics.Color.BLACK;
+import static android.graphics.Color.BLUE;
+import static android.graphics.Color.YELLOW;
 
 public class ChessBoard extends ViewGroup {
 
-    int rowNumber = 9;//行数
+    private Context context;
+    private int rowNumber;//行数
+    private int xSize, ySize;//屏幕尺寸
+    private Paint mPaint;//画笔
+
+    private SudokuBean[][] sudokus;//关卡数据
 
     public ChessBoard(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public ChessBoard(Context context, AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public ChessBoard(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.context = context;
+        setWillNotDraw(false);//调用onDraw方法
+        initData();
+    }
+
+    private void initData() {
+        rowNumber = 9;
+        mPaint = new Paint();
+        sudokus = new SudokuBean[][]{};
+        for (int i = 0; i < rowNumber * rowNumber; i++) {
+            TextView child = new TextView(context);
+            child.setText("ceshi");
+            child.setBackgroundColor(YELLOW);
+            LayoutParams la = new LayoutParams(10, 10);
+            addView(child, -1, la);
+        }
+        requestLayout();
+    }
+
+    //初始化行数
+    public void setRowNumber(int rowNumber) {
+        this.rowNumber = rowNumber;
+    }
+
+    //初始化关卡信息
+    public void setData(SudokuBean[][] sudokuBeans) {
+        this.sudokus = sudokuBeans;
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int sizeSpec = widthMeasureSpec > heightMeasureSpec ? heightMeasureSpec : widthMeasureSpec;
+        super.onMeasure(sizeSpec, sizeSpec);
+        Log.e("屏幕尺寸1", "widthMeasureSpec>>>" + widthMeasureSpec + ",heightMeasureSpec>>>" + heightMeasureSpec);
+
+        xSize = getMeasuredWidth();
+        ySize = getMeasuredHeight();
+        Log.e("屏幕尺寸2", "measuredWidth>>>" + getMeasuredWidth() + ",measuredHeight>>>" + getMeasuredHeight());
+
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        Log.e("摆放", "changed>>>" + changed + ",l>>>" + l + ",t>>>" + t + ",r>>>" + r + ",b>>>" + b);
+
+        int perWithSize = xSize / rowNumber;
+        int perHeightSize = ySize / rowNumber;
+
+        for (int y = 0; y < rowNumber; y++) {
+            for (int x = 0; x < rowNumber; x++) {
+
+                SudokuBean sudokuBean = sudokus[y][x];
+//                Log.e("", "sudokuBean>>>" + sudokuBean.toString());
+
+                int childCount = getChildCount();
+                Log.e("", "子元素的数量>>>" + childCount);
+
+//                TextView tv = (TextView) getChildAt(y * rowNumber + x);
+//                MarginLayoutParams params = (MarginLayoutParams) tv.getLayoutParams();
+//                tv.setHeight(perHeightSize);
+//                tv.setWidth(perWithSize);
+//                tv.setX(sudokuBean.getX() * perWithSize);
+//                tv.setY(sudokuBean.getY() * perHeightSize);
+//                tv.setText("" + sudokuBean.getValue());
+//                tv.setBackgroundColor(YELLOW);
+//                tv.setTextColor(BLACK);
+//                LayoutParams params = generateDefaultLayoutParams();
+//                params.width = perWithSize;
+//                params.height = perHeightSize;
+
+            }
+        }
 
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        //背景色
+//        canvas.drawColor(GREEN);
 
+        int perWithSize = xSize / rowNumber;
+        int perHeightSize = ySize / rowNumber;
+
+        //画网格
+        mPaint.setColor(BLUE);
+        mPaint.setStrokeWidth(3);
+
+        //画横线
+        for (int i = 0; i <= rowNumber; i++) {
+            canvas.drawLine(0, perHeightSize * i, xSize, perHeightSize * i, mPaint);
+        }
+        //画竖线
+        for (int i = 0; i <= rowNumber; i++) {
+            canvas.drawLine(perWithSize * i, 0, perWithSize * i, ySize, mPaint);
+        }
     }
 }
